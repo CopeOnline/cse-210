7d948215c79a6aad38a9c0cf2e9c673e188c3bfd
CopeOnline <Cop21004@byui.edu>
2024-06-07 19:06:01 -0600

    Finished prep 1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -6,12 +6,12 @@ class Program
     static void Main(string[] args)
     {
         Console.Write("What is your first name? ");
-        string f_name = Console.ReadLine();
+        string fName = Console.ReadLine();
 
         Console.Write("What is your last name? ");
-        string l_name = Console.ReadLine();
-        
-        Console.Write($"Your name is {l_name}, {f_name} {l_name}.");   
+        string lName = Console.ReadLine();
+
+        Console.Write($"Your name is {lName}, {fName} {lName}.");   
         
     }
 }

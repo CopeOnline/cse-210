d2934295de9563805f924ed33911db6351cc40f4
CopeOnline <Cop21004@byui.edu>
2024-06-07 20:12:56 -0600

    Finished prep 2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        int magicNumber = int.Parse(Console.ReadLine());
+
+        Console.Write("What is your guess? ");
+        int userGuess = int.Parse(Console.ReadLine());
+
+        if (magicNumber > userGuess)
+        {
+            Console.Write("Higher");
+        }
+        else if (magicNumber < userGuess)
+        {
+            Console.Write("Lower");
+        }
+        else 
+        {
+            Console.Write("You guessed it!");
+        }
     }
 }

4fb6a49b4a1991dad9a245639ad0af3733085942
CopeOnline <Cop21004@byui.edu>
2024-06-07 20:37:56 -0600

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -13,8 +13,11 @@ class Program
         string found = "no";
         int count = 0;
 
-        Console.Write("What is the magic number? ");
-        int magicNumber = int.Parse(Console.ReadLine());
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
 
         while (found == "no")
         {

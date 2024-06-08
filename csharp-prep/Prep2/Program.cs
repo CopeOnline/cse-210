b7bcfe62dd18fc0188af2131176c38bcf7879c8e
CopeOnline <Cop21004@byui.edu>
2024-06-07 19:53:15 -0600

    Finished prep 1

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,59 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter your grade: ");
+        string response = Console.ReadLine();
+
+        int grade = int.Parse(response);
+
+        int sign = grade % 10;
+
+        string letter;
+        string mod;
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        if (sign >= 7)
+        {
+            mod = "+";
+        }
+        else if (sign < 3)
+        {
+            mod = "-";
+        }
+        else
+        {
+            mod = null;
+        }
+
+        if (grade >= 97 && grade < 73)
+        {
+            mod = null;
+        }
+
+        Console.Write($"Your grade is {letter}{mod}");
     }
 }

270000de0edd2b541e5edcf4fef056a472b6a414
CopeOnline <Cop21004@byui.edu>
2024-06-07 20:34:01 -0600

    Finished prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,26 +1,43 @@
 using System;
+using System.Net;
 
 class Program
 {
     static void Main(string[] args)
-    {
+    {   
+        string response = "yes";
+
+        do
+        {
+            
+        string found = "no";
+        int count = 0;
+
         Console.Write("What is the magic number? ");
         int magicNumber = int.Parse(Console.ReadLine());
 
-        Console.Write("What is your guess? ");
-        int userGuess = int.Parse(Console.ReadLine());
+        while (found == "no")
+        {
+            Console.Write("What is your guess? ");
+            int userGuess = int.Parse(Console.ReadLine());
 
-        if (magicNumber > userGuess)
-        {
-            Console.Write("Higher");
+            if (magicNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else 
+            {
+                Console.WriteLine($"You guessed it in {count} tries!");
+                found = "yes";
+                Console.Write("Do you want to play again? ");
+                response = Console.ReadLine();
+            }
+            count += 1;
         }
-        else if (magicNumber < userGuess)
-        {
-            Console.Write("Lower");
-        }
-        else 
-        {
-            Console.Write("You guessed it!");
-        }
+        } while (response == "yes");
     }
 }
